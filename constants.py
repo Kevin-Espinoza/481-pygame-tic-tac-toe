484d9f95db1678c4597adbcfc471b4188a8a9fe2
Michael Trinh <michaeltrinh@csu.fullerton.edu>
2022-12-05 21:15:37 -0800

    moved consts to seperate file

---
+++ constants.py
@@ -0,0 +1,10 @@
+#######################################
+###             Constants           ###
+#######################################
+from main import *
+
+WINDOW_SIZE = 900               # size of game window (900px)
+CELL_SIZE = WINDOW_SIZE // 3    # size of individual cell (300px)
+INF = float('inf')              # variable to hold infinity
+vector2 = pg.math.Vector2          
+CENTER = vector2(CELL_SIZE / 2)    #
